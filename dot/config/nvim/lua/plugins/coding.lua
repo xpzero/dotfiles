69f91aa6e53d7dfa30ab404e93ed2586ad0d6691
kehan <x73mail@gmail.com>
2023-09-21 23:41:22 +0800

    feat: alter luasnip's configure

--- dot/config/nvim/lua/plugins/coding.lua
+++ dot/config/nvim/lua/plugins/coding.lua
@@ -1,10 +1,13 @@
 return {
   {
-    "rafamadriz/friendly-snippets",
-    config = function()
-      require("luasnip.loaders.from_vscode").lazy_load()
-      -- add custom code snippets.
-      require("luasnip.loaders.from_vscode").lazy_load({ paths = { "~/.config/nvim/snippets" } })
-    end
-  }
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+      "rafamadriz/friendly-snippets",
+      config = function()
+        require("luasnip.loaders.from_vscode").lazy_load()
+        -- add custom code snippets.
+        require("luasnip.loaders.from_vscode").lazy_load({ paths = { "~/.config/nvim/snippets" } })
+      end,
+    },
+  },
 }
