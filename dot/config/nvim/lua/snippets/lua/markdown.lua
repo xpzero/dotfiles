1fd9b4b4b232fbb123be4bda9229afad32f722d2
kehan <x73mail@gmail.com>
2023-09-24 21:14:31 +0800

    feat: add tabs's snippets in markdown

--- dot/config/nvim/lua/snippets/lua/markdown.lua
+++ dot/config/nvim/lua/snippets/lua/markdown.lua
@@ -1,23 +1,42 @@
 local ls = require("luasnip")
 local fmt = require("luasnip.extras.fmt").fmt
-ls.add_snippets("markdown", {
-  ls.snippet(
-    "tt",
-    fmt(
-      [[ 
+
+local tt = ls.snippet(
+  "tt",
+  fmt(
+    [[ 
     ---
     title: {xxx}
     ---
     ]],
-      {
-        xxx = ls.function_node(function()
-          -- :t get trailing string with extension.
-          -- :r get the string of before extension.
-          -- return vim.fn.fnamemodify(vim.fn.bufname(), ":t:r")
-          -- or below. source: https://neovim.io/doc/user/builtin.html#expand()
-          return vim.fn.expand("%:t:r")
-        end),
-      }
-    )
-  ),
-})
+    {
+      xxx = ls.function_node(function()
+        -- :t get trailing string with extension.
+        -- :r get the string of before extension.
+        -- return vim.fn.fnamemodify(vim.fn.bufname(), ":t:r")
+        -- or below. source: https://neovim.io/doc/user/builtin.html#expand()
+        return vim.fn.expand("%:t:r")
+      end),
+    }
+  )
+)
+
+local tabs = ls.snippet(
+  "tabs",
+  fmt(
+    [[
+<Tabs defaultValue="{}">
+  <TabItem value="{}">
+{}
+  </TabItem>
+</Tabs>
+]],
+    {
+      ls.insert_node(1),
+      ls.insert_node(2),
+      ls.insert_node(3),
+    }
+  )
+)
+
+ls.add_snippets("markdown", { tt, tabs })
