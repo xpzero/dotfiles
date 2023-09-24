dbc2ab6b77009c5b7c9c46181c8a93ebe5a7bebd
kehan <x73mail@gmail.com>
2023-09-24 14:54:35 +0800

    feat: add another method about getting buffer's name

--- dot/config/nvim/lua/luaSnippets/markdown.lua
+++ dot/config/nvim/lua/luaSnippets/markdown.lua
@@ -13,7 +13,9 @@ ls.add_snippets("markdown", {
         xxx = ls.function_node(function()
           -- :t get string with extension at tail.
           -- :r get the string of before extension.
-          return vim.fn.fnamemodify(vim.fn.bufname(), ":t:r")
+          -- return vim.fn.fnamemodify(vim.fn.bufname(), ":t:r")
+          -- or below. source: https://neovim.io/doc/user/builtin.html#expand()
+          return vim.fn.expand("%:t:r")
         end),
       }
     )
