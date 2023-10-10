dc42ab14942e39ba362a854ca3497277d9279a0d
kehan <x73mail@gmail.com>
2023-10-10 23:26:08 +0800

    feat: add Y mapping in neo-tree

--- dot/config/nvim/lua/plugins/editor.lua
+++ dot/config/nvim/lua/plugins/editor.lua
@@ -17,20 +17,40 @@ return {
         filtered_items = {
           hide_dotfiles = false,
         },
+        commands = {
+          copy_selector = function(state)
+            local node = state.tree:get_node()
+            local filepath = node:get_id()
+            local filename = node.name
+            local modify = vim.fn.fnamemodify
+
+            local results = {
+              filepath,
+              modify(filepath, ":."),
+              modify(filepath, ":~"),
+              filename,
+              modify(filename, ":r"),
+              modify(filename, ":e"),
+            }
+
+            vim.ui.select({
+              "1. Absolute path: " .. results[1],
+              "2. Path relative to CWD: " .. results[2],
+              "3. Path relative to HOME: " .. results[3],
+              "4. Filename: " .. results[4],
+              "5. Filename without extension: " .. results[5],
+              "6. Extension of the filename: " .. results[6],
+            }, { prompt = "Choose to copy to clipboard:" }, function(choice)
+              local i = tonumber(choice:sub(1, 1))
+              local result = results[i]
+              vim.fn.setreg("+", result)
+              vim.notify("Copied: " .. result)
+            end)
+          end,
+        },
         window = {
           mappings = {
-            ["y"] = function(state)
-              local node = state.tree:get_node()
-              local full_path = node.path
-
-              vim.fn.setreg("*", full_path, "c")
-            end,
-            ["<c-y>"] = function(state)
-              local node = state.tree:get_node()
-              local full_path = node.path
-              local relative_path = full_path:sub(#state.path + 2)
-              vim.fn.setreg("*", relative_path, "c")
-            end,
+            Y = "copy_selector",
             h = "close_node",
             l = "open",
           },
