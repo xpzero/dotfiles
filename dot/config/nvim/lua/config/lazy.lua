5ee642e29c861f20b5777c6c78acc150918190a6
kehan <x73mail@gmail.com>
2024-09-12 01:55:44 +0800

    feat: add mini-surround extra

--- dot/config/nvim/lua/config/lazy.lua
+++ dot/config/nvim/lua/config/lazy.lua
@@ -14,6 +14,7 @@ require("lazy").setup({
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.coding.codeium" },
+    { import = "lazyvim.plugins.extras.coding.mini-surround" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     -- { import = "lazyvim.plugins.extras.linting.eslint" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
