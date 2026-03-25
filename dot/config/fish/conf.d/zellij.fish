33d608ea72ba19a761dc3188c1bef8cf876c6597
徐培珊 <xps01671714@lydaas.com>
2026-03-25 10:05:10 +0800

    feat: zellij alias

---
+++ dot/config/fish/conf.d/zellij.fish
@@ -0,0 +1,8 @@
+# Zellij aliases
+alias zj='zellij'
+alias za='zellij attach'
+alias zls='zellij list-sessions'
+alias zk='zellij kill-session'
+alias zka='zellij kill-all-sessions'
+alias zd='zellij delete-session'
+alias zda='zellij delete-all-sessions'
